ec11d729d122ee87d3f601238cbff41f7b148d8d
jenbazy <jenna.basnett@live.com>
2016-04-02 13:21:53 -0400

    navfix4

--- otherProjects/rootJS.js
+++ otherProjects/rootJS.js
@@ -1,5 +1,5 @@
 //get the file (replace with your own url)
-$.get("http://jenbazy.github.io/otherProjects/nav.html", function (data) {
+$.get("jenbazy.github.io/otherProjects/nav", function (data) {
 
     $(document).ready(function () {
 
