52fb9fe10f7992036687d2ae24b3c18b8814e5e4
jenbazy <jenna.basnett@live.com>
2016-04-02 13:13:22 -0400

    navFix1

--- otherProjects/rootJS.js
+++ otherProjects/rootJS.js
@@ -1,12 +1,17 @@
-$.get("http://jenbazy.github.io/otherProjects/nav.html", function(data) {
+//get the file (replace with your own url)
+$.get("http://jenbazy.github.io/otherProjects/nav.html", function (data) {
 
-  $(document).ready(function() {
-	//check if this document has .container
-	var container = $(".container");
-	//if so add nav.html contents to the top
-	if (container) {
-  	container.prepend(data)
-  	//container.fadeIn();
-	}
-  });
+    $(document).ready(function () {
+
+        //check if this document has .container
+        var container = $(".container");
+
+        //if so add nav.html contents to the top
+        if (container) {
+            container.prepend(data)
+            container.fadeIn();
+        }
+
+    });
+
 });
