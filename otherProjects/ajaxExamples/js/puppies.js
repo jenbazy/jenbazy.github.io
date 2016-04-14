a9ff14cd0f5b3dba4a913dbdc8328c8d136e4b28
jenbazy <jenna.basnett@live.com>
2016-04-14 10:18:03 -0400

    puppyPics

--- otherProjects/ajaxExamples/js/puppies.js
+++ otherProjects/ajaxExamples/js/puppies.js
@@ -5,24 +5,36 @@ $(document).ready(function () {
                 var url = "http://jenbazy.github.io/otherProjects/ajaxExamples/jsonDatabase/puppies.json";
 
                 $.getJSON(url, function (data) {
+                        var puppies = '<div id="puppies">';
                         var html = "<table class='table table-hover table-striped'>" +
                             "<tr><th>Breed</th><th>Colours</th><th>Temperament</th></tr>";
+                        
 
                         $.each(data, function (index, item) {
+                            puppies += '<img class="puppyPics" src="' + item.image + '"/>' ;
 
                             html += "<tr>" +
                                 "<td>" + item.breed + "</td>" +
                                 "<td>" + item.colors + "</td>" +
                                 "<td>" + item.temperament + "</td>" +
                                 "</tr>";
+                            
                         })
-
+                            
+                        
+                        
+                    
+                        puppies +="</div>"
+                        $("#puppies").append(puppies);
+                        
                         html += "</table>";
                         $("#data").append(html);
+                        
                         //alert(data);
                         //console.dir(data);
 
-                    }) //getJSON
+                    })
+                //getJSON
 
             }) //click
 
