ad8f2c83bf8d16d26810b3cbdd872c0f926ff75b
jenbazy <jenna.basnett@live.com>
2016-04-13 23:21:01 -0400

    puppies.json

---
+++ otherProjects/ajaxExamples/js/puppies.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    
+        $("#getPuppies").on("click", function () {
+
+                var url = "http://jenbazy.github.io/otherProjects/ajaxExamples/jsonDatabase/puppies.json";
+
+                $.getJSON(url, function (data) {
+                        var html = "<table class='table table-hover table-striped'>" +
+                            "<tr><th>Breed</th><th>Colours</th><th>Impressions</th></tr>";
+
+                        $.each(data, function (index, item) {
+
+                            html += "<tr>" +
+                                "<td>" + item.breed + "</td>" +
+                                "<td>" + item.colors + "</td>" +
+                                "<td>" + item.name + "</td>" +
+                                "</tr>";
+                        })
+
+                        html += "</table>";
+                        $("#data").append(html);
+                        //alert(data);
+                        //console.dir(data);
+
+                    }) //getJSON
+
+            }) //click
+
+    }) //ready
